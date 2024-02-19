d0675c11244c31a8600f1b6b47c14f3540e15763
Kirill <lechnev@mail.ru>
2024-02-20 00:19:31 +0300

    Новый тест

--- 02_TestMainPageLinks.cs
+++ 02_TestMainPageLinks.cs
@@ -1,26 +1,26 @@
-global using NUnit.Framework;
 using OpenQA.Selenium;
+using UI_test.PageObjects;
 
 namespace UI_test
 {
+    [TestFixture]
+    [Description("Проверка работы Главной страницы")]
     public class TestMainPageLinks : Base
     {
-        private readonly By block_franshiza = By.XPath("//div[@class='franchize-slide-chevron']");
-        private readonly By link_franshiza = By.XPath("//div[@id='block-block-32']//a");
-        
         [Test]
+        [Description("Проверка работы ссылки Франшиза в попап меню")]
         public void Test()
         {
-            var blockFranshiza = driver.FindElement(block_franshiza);
-            blockFranshiza.Click();
-            Thread.Sleep(1000);
-            var linkFranshiza = driver.FindElement(link_franshiza);
-            linkFranshiza.Click();
-            Thread.Sleep(2000);
+            var mainPage = new MainPage(driver);
+            mainPage
+            .PopapMenuFranshizaClick()
+            .PopapMenuFranshizaClickLinkFranshiza();
+
             driver.SwitchTo().Window(driver.WindowHandles[1]);
             Thread.Sleep(2000);
             string getUrl = driver.Url;
             Assert.AreEqual("https://fr-tochkafamily.ru/", getUrl);
+            driver.Quit();
         }
     }
 }
--- PageObjects/MainPage.cs
+++ PageObjects/MainPage.cs
@@ -15,24 +15,29 @@ namespace UI_test.PageObjects
         private readonly By _menuMapLink = By.XPath("//ul//li[6]//a");
         private readonly By _menuContactsLink = By.XPath("//li[@class='last leaf']//a");
 
+        private readonly By block_franshiza = By.XPath("//div[@class='franchize-slide-chevron']");
+        private readonly By link_franshizaPopapMenu = By.XPath("//div[@id='block-block-32']//a");
+
         public MainPage(IWebDriver webDriver)
         {
             _webDriver = webDriver;
         }
 
-
+        // Клик на ссылку меню Франшиза
         public void MenuFranshizaLinkClick()
         {
             _webDriver.FindElement(_menuFranshizaLink).Click();
             Thread.Sleep(1000);
         }
 
+        // Клик на ссылку меню Обучение
         public void MenuEducationLinkClick()
         {
             _webDriver.FindElement(_menuEducationLink).Click();
             Thread.Sleep(1000);
         }
 
+        // Клик на ссылку меню Работа
         public JobPage MenuJobLinkClick()
         {
             _webDriver.FindElement(_menuJobLink).Click();
@@ -40,28 +45,48 @@ namespace UI_test.PageObjects
             return new JobPage(_webDriver);
         }
 
+        // Клик на ссылку меню Продукция
         public void MenuProductsLinkClick()
         {
             _webDriver.FindElement(_menuProductsLink).Click();
             Thread.Sleep(1000);
         }
 
+        // Клик на ссылку меню Мероприятия
         public void MenuEventsLinkClick()
         {
             _webDriver.FindElement(_menuEventsLink).Click();
             Thread.Sleep(1000);
         }
 
+        // Клик на ссылку меню Карта салонов
         public void MenuMapLinkClick()
         {
             _webDriver.FindElement(_menuMapLink).Click();
             Thread.Sleep(1000);
         }
 
+        // Клик на ссылку меню Контакты
         public void MenuContactsLinkClick()
         {
             _webDriver.FindElement(_menuContactsLink).Click();
             Thread.Sleep(1000);
+        }
+
+        // Клик на попап меню Франшиза
+        public MainPage PopapMenuFranshizaClick()
+        {
+            _webDriver.FindElement(block_franshiza).Click();
+            Thread.Sleep(1000);
+            return new MainPage(_webDriver);
+        }
+
+        // Клик на ссылку Франшизы в попап меню Франшиза
+        public MainPage PopapMenuFranshizaClickLinkFranshiza()
+        {
+            _webDriver.FindElement(link_franshizaPopapMenu).Click();
+            Thread.Sleep(2000);
+            return new MainPage(_webDriver);
         }
     }
 }
